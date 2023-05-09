250ae9308547c6ab43092d7e5467585038b7ea14
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-05-09 10:33:23 -0700

    created variables, added EventListeners

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+// variables
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggle = document.querySelector('.toggle')
 
+// array of days and months
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+]
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'June',
+  'July',
+  'Aug',
+  'Sept',
+  'Oct',
+  'Nov',
+  'Dec',
+]
+
+// dark mode functionality
+toggle.addEventListener('click', (e) => {
+  const html = document.querySelector('html')
+  if (html.classList.contains('dark')) {
+    html.classList.remove('dark')
+    e.target.innerHTML = 'Dark mode'
+  } else {
+    html.classList.add('dark')
+    e.target.innerHTML = 'Light mode'
+  }
+})
